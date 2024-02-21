a1739e79082f260052d8db6940755c1b4cb42700
Sakshi Lahamge <155747835+Sakshilahamge@users.noreply.github.com>
2024-02-22 02:48:06 +0530

    COMMIT

---
+++ C++/Run.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+int main () {
+    int a = 0;
+    while (a<10) {
+    {
+    	while (a%2 == 0){
+		
+        std::cout<<a;
+        a++;
+    }
+    }
+    return 0;
+}
+}
---
+++ C++/even.cc
@@ -0,0 +1,18 @@
+#include <i>
+using namespace;
+
+int()
+{
+        int number = 1;
+
+        while (number 10)
+        {
+                if (number % 2 == )
+                {
+        cout << number << " ";
+  }
+                number++;
+        }
+
+        return 0;
+}
